d7d8b49f972068f767b2abd127965994f518bc68
Alex Dietz <62934780+alex-dietz@users.noreply.github.com>
2023-08-16 22:31:44 +0200

    Revert "firebase init"
    This reverts commit b303992d3586c31643653a9b04db440cb71da5a4.

--- frontend/src/app/core/auction.service.ts
+++ frontend/src/app/core/auction.service.ts
@@ -1,26 +1,21 @@
-import { Injectable, inject } from '@angular/core';
-import { CollectionReference, DocumentData, Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuctionService {
   auctions: any = [];
-  auctionCollection: any;
   auctions$: any;
-  constructor(private firestore: Firestore) {
-    const auctionCollection = collection(this.firestore, 'auctions');
+  constructor() {
     this.auctions$ = new BehaviorSubject<any>(null);
     this.auctions$.next(this.auctions);
   }
 
   createNewAuction(auction: any) {
-    return addDoc(this.auctionCollection, auction);
+    this.auctions.push(auction);
   }
   getAuctions() {
-    return collectionData(this.auctionCollection, {
-      idField: 'id'
-    }) as Observable<any[]>;
+    return this.auctions$.asObservable();
   }
 }
