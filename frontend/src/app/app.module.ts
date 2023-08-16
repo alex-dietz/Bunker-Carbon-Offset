b303992d3586c31643653a9b04db440cb71da5a4
Alex Dietz <62934780+alex-dietz@users.noreply.github.com>
2023-08-16 22:12:57 +0200

    firebase init

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,22 @@ import { AppComponent } from './app.component';
 import { AuctionComponent } from './auction/auction.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CertificatesComponent } from './certificates/certificates.component';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDG-YaETUA-w8MSz5BO1ryr506kAKvgZQc',
+  authDomain: 'bunker-carbon-offset.firebaseapp.com',
+  databaseURL: 'https://bunker-carbon-offset-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'bunker-carbon-offset',
+  storageBucket: 'bunker-carbon-offset.appspot.com',
+  messagingSenderId: '503462480645',
+  appId: '1:503462480645:web:8a9c40c170c2591856056c'
+};
 
 @NgModule({
   declarations: [AppComponent, CertificatesComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, provideFirebaseApp(() => initializeApp(firebaseConfig)), provideFirestore(() => getFirestore())],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/core/auction.service.ts
+++ frontend/src/app/core/auction.service.ts
@@ -1,21 +1,26 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, inject } from '@angular/core';
+import { CollectionReference, DocumentData, Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuctionService {
   auctions: any = [];
+  auctionCollection: any;
   auctions$: any;
-  constructor() {
+  constructor(private firestore: Firestore) {
+    const auctionCollection = collection(this.firestore, 'auctions');
     this.auctions$ = new BehaviorSubject<any>(null);
     this.auctions$.next(this.auctions);
   }
 
   createNewAuction(auction: any) {
-    this.auctions.push(auction);
+    return addDoc(this.auctionCollection, auction);
   }
   getAuctions() {
-    return this.auctions$.asObservable();
+    return collectionData(this.auctionCollection, {
+      idField: 'id'
+    }) as Observable<any[]>;
   }
 }
